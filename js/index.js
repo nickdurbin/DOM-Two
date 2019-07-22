95784b6485d2a2827a5a472737771b3f0ef96fe0
durbcodes <durbcodes@gmail.com>
2019-07-22 10:34:35 -0400

    Added events on the main navigation for a toggle color effect.

--- js/index.js
+++ js/index.js
@@ -1,31 +1,36 @@
-// eventListners
+// eventListeners
 
-addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('load', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('load', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('focus', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('focus', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('resize', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('resize', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('select', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('select', (event) => { event.target.style.backgroundColor = 'blue';});
 
-addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'blue';});
+// addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'blue';});
 
 
 // Navigation eventListener and refresh prevention
-let mainNav = document.querySelector('.main-navigation');
-mainNav.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = 'darkgray';});
+let stopPropHandler = (event) => { event.stopPropagation() };
+let preventHandler = (event) => { event.preventDefault() };
 
-mainNav.addEventListener('mouseover', (event) => { event.preventDefault()});
+let mainNav = document.querySelector('.main-navigation');
+
+mainNav.addEventListener('mouseover', (event) => { event.currentTarget.style.backgroundColor = 'gray'; });
+
+mainNav.addEventListener('mouseout', (event) => { event.currentTarget.style.backgroundColor = 'white'; });
 
 // Stop propagation from bubbling
-addEventListener('mouseover', (event) => { event.stopPropagation()});
+
+// let stopPropHandler = (event) => { event.stopPropagation() };
 
 // GSAP interactions
