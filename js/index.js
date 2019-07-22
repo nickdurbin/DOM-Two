abc1263de56bfd496f9a374e037b77925b66cee4
ndurbin <nick.durbin@gmail.com>
2019-07-21 20:21:21 -0400

    Initialized LESS and added all the event listeners, propagation, and prevent methods.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+// 10 eventListners
+addEventListener('mouseover', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('load', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('focus', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('resize', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('select', (event) => { event.target.style.backgroundColor = 'blue';});
+
+addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'blue';});
+
+
+// Navigation refresh prevention
+addEventListener('mouseover', (event) => { event.preventDefault()});
+
+// Stop propagation from bubbling
+addEventListener('mouseover', (event) => { event.stopPropagation()});
+
+// GSAP interactions
