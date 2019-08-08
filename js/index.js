4e1b63b812cf6e57ecfd9fc418fd84e16879689e
nick.durbin <nick.durbin@gmail.com>
2019-08-08 16:39:10 -0400

    Bringing over the work from the previous repo.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-// Your code goes here
+// eventListeners
+
+// addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('load', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('focus', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('resize', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('select', (event) => { event.target.style.backgroundColor = 'blue';});
+
+// addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'blue';});
+
+
+// Navigation eventListener and refresh prevention
+let stopPropHandler = (event) => { event.stopPropagation() };
+let preventHandler = (event) => { event.preventDefault() };
+
+let mainNav = document.querySelector('.main-navigation');
+
+mainNav.addEventListener('mouseover', (event) => { event.currentTarget.style.backgroundColor = 'gray'; });
+
+mainNav.addEventListener('mouseout', (event) => { event.currentTarget.style.backgroundColor = 'white'; });
+
+// Stop propagation from bubbling
+
+// let stopPropHandler = (event) => { event.stopPropagation() };
+
+// GSAP interactions
