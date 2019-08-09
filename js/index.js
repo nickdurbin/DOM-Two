6420de9ad306ead3b7f36f93e21b34c167e2ff7e
ndurbin <nick.durbin@gmail.com>
2019-08-09 17:13:47 -0400

    Added some comments to understand what each event listener is doing.

--- js/index.js
+++ js/index.js
@@ -13,35 +13,44 @@ const fun = document.querySelector('#fun');
 const anchor = document.querySelectorAll('.nav a');
 const destination = document.querySelectorAll('.destination');
 
-
 // 10 Event Listeners
 
-// Anchor tags with the prevent default forEach method
+// Anchor tags enlarge onclick with the prevent default forEach method
 anchor.forEach(tag => tag.addEventListener('click', (event) => {
   event.preventDefault()
   event.target.style.fontSize = '2.8rem';
 }));
 
+// Paragraph elements turn to purple on mouseleave
 paragraph.forEach(tag => tag.addEventListener('mouseleave', (event) => {
   event.target.style.color = 'purple';
 }));
 
+// The font truns green on keydown
 window.addEventListener('keydown', (event) => { event.target.style.color = 'green';});
 
+// The font turns back to black on keyup
 window.addEventListener('keyup', (event) => { event.target.style.color = 'black';});
 
+// Using mouseenter we turn the background yellow on the headline intro
 intro.addEventListener('mouseenter', (event) => { event.target.style.backgroundColor = 'yellow';});
 
+// Using mouseover with currentTarget we turn the background gray of the nav element
 mainNav.addEventListener('mouseover', (event) => { event.currentTarget.style.backgroundColor = 'gray'; });
 
+// Using mouseout we turn the nav element back to white
 mainNav.addEventListener('mouseout', (event) => { event.currentTarget.style.backgroundColor = 'white'; });
 
+// Mousedown increases the font size of the header in the Fun section
 fun.addEventListener('mousedown', (event) => { event.target.style.fontSize = '4rem'; });
 
+// Double clicking the mouse will change the text in the Footer to 'Changed'
 footer.addEventListener('dblclick', (event) => { event.target.innerHTML = 'Changed';});
 
+// Click the mouse will enlarge the button 1.2x
 button.addEventListener('click', (event) => { event.target.style.transform = 'scale(1.2)';});
 
+// Running through the nodelist of destination elements we change the background to light blue and keep it from propagating to other elements
 destination.forEach(section => section.addEventListener('click', (event) => {
   event.stopPropagation()
   event.target.style.backgroundColor = "lightblue";
@@ -54,6 +63,6 @@ tl.fromTo(content, 1, { height: "0%" }, { height: "90%", ease: Power2.easeInOut 
   .fromTo(content, 1.2, { width: "100%" }, { width: "90%", ease: Power2.easeInOut })
   .fromTo(nav, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.3")
 
-// Propgation and Prevent Handler Variables
+// Propgation and Prevent Handler Variables (not used)
 let stopPropHandler = (event) => { event.stopPropagation() };
 let preventHandler = (event) => { event.preventDefault() };
