6455dae6def362c8e6d17a985b6f7a413430ac92
ndurbin <nick.durbin@gmail.com>
2019-08-08 22:22:23 -0400

    Added most of the generic eventListeners and the prop/prevent handlers.

--- js/index.js
+++ js/index.js
@@ -1,50 +1,38 @@
-// eventListeners
+// Variables
+const firstParagraph = document.querySelectorAll('p')[0];
+const mainNav = document.querySelector('.main-navigation');
+const intro = document.querySelector('.intro h2');
+const hero = document.querySelector('.intro img');
+const header = document.querySelector('.logo-heading');
+const text = document.querySelector('.text-content');
+const button = document.querySelector('.btn');
+const content= document.querySelector('.container');
+const nav = document.querySelector('.nav');
 
-// addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'blue';});
+// 10 Event Listeners
+firstParagraph.addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'green';});
 
-// addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'blue';});
-
-// addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
-
-// addEventListener('load', (event) => { event.target.style.backgroundColor = 'blue';});
-
-// addEventListener('focus', (event) => { event.target.style.backgroundColor = 'blue';});
-
-
-
-// addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'blue';});
-
-// addEventListener('select', (event) => { event.target.style.backgroundColor = 'blue';});
-
-// addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'blue';});
-
-
-// Navigation eventListener and refresh prevention
-let stopPropHandler = (event) => { event.stopPropagation() };
-let preventHandler = (event) => { event.preventDefault() };
-
-let mainNav = document.querySelector('.main-navigation');
+intro.addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'yellow';});
 
 mainNav.addEventListener('mouseover', (event) => { event.currentTarget.style.backgroundColor = 'gray'; });
 
 mainNav.addEventListener('mouseout', (event) => { event.currentTarget.style.backgroundColor = 'white'; });
 
-// Resize the Heading Font 
-const intro = document.querySelector('.intro h2');
-intro.addEventListener('scroll', (event) => { event.target.style.fontSize = '5rem'; });
+text.addEventListener('scroll', (event) => { event.target.style.fontSize = '5rem'; });
 
-const hero = document.querySelector('.intro img');
-hero.addEventListener('focusin', (event) => { event.target.style.backgroundColor = ''; });
+hero.addEventListener('focus', (event) => { event.target.style.backgroundColor = 'green'; });
 
-// Stop propagation from bubbling
+addEventListener('drag / drop', (event) => { event.target.style.backgroundColor = 'blue';});
 
-// let stopPropHandler = (event) => { event.stopPropagation() };
+btn.addEventListener('click', (event) => { event.target.alert = 'You Clicked Me';});
+
+btn.addEventListener('dblclick', (event) => { event.target.innerHTML = 'Hello';});
+
+// Propgation and Prevent Handler Variables
+let stopPropHandler = (event) => { event.stopPropagation() };
+let preventHandler = (event) => { event.preventDefault() };
 
 // GSAP interactions
-const content= document.querySelector('.container');
-const nav = document.querySelector('.nav');
-
-// Animations for the navigation using Tween and Timeline
 const tl = new TimelineMax();
 
 tl.fromTo(content, 1, { height: "0%" }, { height: "90%", ease: Power2.easeInOut })
