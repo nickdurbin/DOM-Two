fa366579bf6834073ca6253ac1da37efc7f3324f
ndurbin <nick.durbin@gmail.com>
2019-08-09 18:09:25 -0400

    Working on the Stretch problem. I have most of the logic complete, but definitely missing something.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,54 +1,58 @@
 function moveRocket() {
-  let leftPosition = 0; 
-  let rocket = document.querySelector('.block');
+  let rocket = document.querySelectorAll('.block');
+  let leftPosition = 0;
   let time = setInterval(moveLeft, 10);
 
-  function moveLeft() {
-      if(leftPosition >= 150) {
+  rocket.forEach(grab => grab.addEventListener('mouseup', (event) => event.target = function moveLeft() {
+      if(leftPosition >= 1000) {
           clearInterval(time);
-          time = setInterval(moveDown, 5);
+          time = setInterval(moveRight, 5);
       }
       else {
           leftPosition += 1;
           rocket.style.left = leftPosition+'px';
       }
-  }
+  }))
   
-  bottomPosition = 1000;
+  let bottomPosition = 1000;
   
-  function moveDown(){
+    rocket.forEach(grab => grab.addEventListener('click', (event) =>
+        event.target = function moveDown(){
       if(bottomPosition == 0){
           clearInterval(time);
-          time = setInterval(moveRight, 10);
+          time = setInterval(moveUp, 1000);
       }
       else{
           bottomPosition -= 1;
           rocket.style.bottom = bottomPosition+'px';
       }
-  }
+  }))
   
   let rightPosition = 1000;
   
-  function moveRight() {
-     if(rightPosition == 0) {
+  rocket.forEach(grab => grab.addEventListener('mousedown', (event) =>
+    event.target = function moveRight() {
+     if(rightPosition == 1000) {
          clearInterval(time);
-         time = setInterval(moveUp, 20);
+         time = setInterval(moveLeft, 20);
      }
      else {
          rightPosition -= 1;
          rocket.style.left = rightPosition+'px';
      }
-  }
+  }))
   
    let topPosition = 1000;
   
   function moveUp(){
-      if(topPosition == 0) {
-         clearInterval(time);
-      }
-      else{
-         topPosition -= 1;
-         rocket.style.top = topPosition+'px';
-      }
+    if(topPosition == 0) {
+        clearInterval(time);
+    }
+    else{
+        topPosition -= 1000;
+        rocket.style.top = topPosition+'px';
+    }
   }
 };
+
+moveRocket();
