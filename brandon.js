20e1cb9284dbe3daf46e9ed5c0954572384b856a
nick.durbin <nick.durbin@gmail.com>
2020-01-22 09:17:26 -0500

    Solution

--- brandon.js
+++ brandon.js
@@ -1,25 +1,12 @@
 const boxes = document.querySelectorAll(".box")
-const overlay = document.querySelector(".box_overlay")
+const overlays = document.querySelectorAll(".box_overlay")
 
-
-
-boxes.forEach(box => box.addEventListener('mouseover', (event) => {
-    event.currentTarget.style.childElement('.box_overlay').height = '87px'
-    // overlay.style.height = '87px';
-    // overlay.style.backgroundColor = '#3168a6'; 
+boxes.forEach((box, i) => box.addEventListener('mouseover', (event) => {
+   overlays[i].style.height = '87px';
+   overlays[i].style.backgroundColor = '#3168a6'; 
 }))
 
-// boxes.forEach(box => box.addEventListener('mouseover', (event) => {
-//   event.stopPropagation()
-//   event.target.style.height = "100px"
-// }))
-
-// boxes.forEach(box => box.addEventListener('mouseout', (event) => {
-//   event.stopPropagation()
-//   event.target.style.height = "200px"
-// }))
-
-boxes.forEach(box => box.addEventListener('mouseout', () => {
-    overlay.style.height = '35px';
-    overlay.style.backgroundColor = '#0a304e'; 
+boxes.forEach((box, i) => box.addEventListener('mouseout', (event) => {
+   overlays[i].style.height = '35px';
+   overlays[i].style.backgroundColor = '#0a304e'; 
 }))
